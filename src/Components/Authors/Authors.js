4236d5aae5631a1c78208cc3e2a3f57edd890cca
sanaul01 <mdsanaulislam154@gmail.com>
2021-09-26 00:46:24 +0600

    first commit

---
+++ src/Components/Authors/Authors.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Authors.css'
+
+const Authors = () => {
+    return (
+        <div>
+            <h3>authors</h3>
+        </div>
+    );
+};
+
+export default Authors;
